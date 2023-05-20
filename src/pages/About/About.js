dd1d4f67e6998197dc8c15238fa2a5577e93ddc0
RazelRaz <razel16dcm@gmail.com>
2023-05-20 11:02:10 -0700

    BreadCrumb area done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './About.css'
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+import AboutHero from '../../images/abouthero.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>Im About</h2>
-        </div>
+        <>
+            <BreadCrumb title="About Us" image={AboutHero}>
+            You’re going to have to let it hurt. Let it suck. The harder you work, the better you will look. Your appearance isn’t parallel to how heavy you lift, it’s parallel to how hard you work.
+            </BreadCrumb>
+        </>
     );
 };
 
