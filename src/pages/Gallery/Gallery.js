a18200895a91925adbb6e9107fcbf91a9f5b7a78
RazelRaz <razel16dcm@gmail.com>
2023-05-21 04:56:05 -0700

    gallery page done

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Gallery.css'
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+import GymHead from '../../images/gymGalleryHead.jpg';
+import GalleryImg from '../../components/GalleryImg/GalleryImg';
 
 const Gallery = () => {
+    
+
     return (
-        <div>
-            Gallery
-        </div>
+        <>
+            <BreadCrumb title="Gallery" image={GymHead}>
+            Physical and mental fitness is of utmost importance. Getting to the gym for workout and prioritizing your fitness isn’t easy.
+            </BreadCrumb>
+            <GalleryImg></GalleryImg>
+        </>
     );
 };
 
