0702f453724c92a31c75a44604af2965179c2b27
RazelRaz <razel16dcm@gmail.com>
2023-05-22 07:28:55 -0700

    Page not found comp and css done

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './NotFound.css'
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+import contactUsImage from '../../images/contact.jpg';
+import NothingFound from '../../components/NothingFound/NothingFound';
 
 const NotFound = () => {
     return (
-        <div>
-             <h1>NotFound 404</h1>
-        </div>
+        <>
+            <BreadCrumb title="Not Found" image={contactUsImage}>
+                No Page Found
+            </BreadCrumb>
+            <NothingFound></NothingFound>
+        </>
+        
     );
 };
 
