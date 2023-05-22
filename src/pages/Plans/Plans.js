fbb25414edd75eb1053c4e38f25bac062b907d75
RazelRaz <razel16dcm@gmail.com>
2023-05-22 08:21:30 -0700

    membership plans added into page

--- src/pages/Plans/Plans.js
+++ src/pages/Plans/Plans.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Plans.css'
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+import plansImage from '../../images/plans.jpg';
+import MembershipPlans from '../../components/MembershipPlans/MembershipPlans';
 
 const Plans = () => {
     return (
-        <div>
-            plans
-        </div>
+        <>
+            <BreadCrumb title="Membership Plans" image={plansImage}>
+            At GYMFUEL we believe Crossfitters come in all shapes and sizes, we are all on a journey towards our own personal best health and fitness levels.
+            </BreadCrumb>
+            <MembershipPlans></MembershipPlans>
+        </>
     );
 };
 
