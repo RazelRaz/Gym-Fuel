5a7becb4fb621dc79fc4323f209450c09b9189a1
RazelRaz <razel16dcm@gmail.com>
2023-05-24 08:11:07 -0700

    Trainers area done, all media query checked done. All pages checked

--- src/pages/Trainers/Trainers.js
+++ src/pages/Trainers/Trainers.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Trainers.css'
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+import trainers from '../../images/trainers.jpg';
+import Instructors from '../../components/Instructors/Instructors';
+
 
 const Trainers = () => {
     return (
-        <div>
-            Trainers
-        </div>
+        <>
+            <BreadCrumb title="Our Instructors" image={trainers}>
+            GYMFUEL's goal is to help people understand there are simple ways to keep your body healthy, toned and active. We provide all the tools and support you need to achieve this.​
+            </BreadCrumb>
+            <Instructors></Instructors>
+        </>
     );
 };
 
